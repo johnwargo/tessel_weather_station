cc4d580675be17a333badb4ecc244fd453e512c9
John M. Wargo <john@johnwargo.com>
2016-09-11 16:20:53 -0400

    updated server.js

--- server.js
+++ server.js
@@ -13,30 +13,53 @@ const ACTIVITY_LED = 3;
 const MODULE_PORT = 'A';
 const MEASUREMENT_INTERVAL = 15; //minutes
 // Weather Underground constants
-const WU_URL = 'http://weatherstation.wunderground.com/weatherstation/updateweatherstation.php';
-// todo: Move these to an external file for account protection
-const WU_STATION_ID = 'KNCCHARL225';
-const WU_STATION_KEY = 'I4UL3GPT';
+const WU_URL = 'http://weatherstation.wunderground.com/weatherstation/updateweatherstation.php?';
 
-// Import the interface to Tessel hardware
-var tessel = require('tessel');
-var climate = climatelib.use(tessel.port[MODULE_PORT]);
-// Load the http module to create an http server.
-var http = require('http');
-var fs = require('fs');
-var os = require('os');
-var path = require('path');
-var url = require('url');
+const mimeTypes = {
+  "html": "text/html",
+  "jpeg": "image/jpeg",
+  "jpg": "image/jpeg",
+  "png": "image/png",
+  "js": "text/javascript",
+  "css": "text/css"
+};
+
+//===============================================
+// Project-specific requires
+//===============================================
+//Load the server's external configuration file
+const Config = require('./config.js');
+// Import the Tessel hardware library
+const tessel = require('tessel');
+//load the Tessel climate module library
+const climatelib = require('climate-si7020');
+const climate = climatelib.use(tessel.port[MODULE_PORT]);
+//Other node modules used by this app
+const http = require('http');
+const fs = require('fs');
+const os = require('os');
+const path = require('path');
+const querystring = require('querystring');
+const url = require('url');
 
 // variables used to store last measurements for web app access
 var lastData = {};
 //populate the lastData object
 lastData.temp = 0;
 lastData.humidity = 0;
-lastData.timeStamp = Date.now();
+var d = new Date();
+lastData.timestamp = d.toLocaleString();
 
-//Turn off the Activity LED, just in case it's already on
+//Turn off the Activity LED, just in case it's already on for some reason
 tessel.led[ACTIVITY_LED].off();
+
+//Do we have the Weather Underground configuration values we need?
+if (Config.WU_STATION_ID === '' || Config.WU_STATION_KEY === '') {
+  //Tell the user
+  console.error('\nSTOP: Missing Weather Underground configuration values\n');
+  //get out of here
+  process.exit(1);
+}
 
 // Initialize the web server
 var server = http.createServer(function (request, response) {
@@ -44,46 +67,39 @@ var server = http.createServer(function (request, response) {
   var urlParts = url.parse(request.url, true);
   //is the request /get_data? then return the last measurements object
   if (urlParts.path == '/get_data') {
-    console.log('Processing get_data request');
+    console.log('Processing /get_data request');
     //Write the JSON header to the response
     response.writeHead(200, {"Content-Type": "application/json"});
     //Then send the lastData object with the response
-    response.end(lastData);
+    response.end(JSON.stringify(lastData));
   } else {
-    console.log('Serving resource request');
     //some of this code 'borrowed' from http://stackoverflow.com/questions/7268033/basic-static-file-server-in-nodejs
     // Serve whatever file the requester needs, otherwise return an error
     var reqPath = urlParts.path;
-    console.log('Path: %s', reqPath);
     //Was the root of the server app requested? Then serve the index.html file
-    reqPath = (reqPath == '/') ? 'index.html' : reqPath;
-    console.log('Request path: %s', reqPath);
-    // if (reqPath == '/') {
-    //   reqPath = 'index.html';
-    // }
+    reqPath = (reqPath == '/') ? '/index.html' : reqPath;
     // get a path pointing to the requested file
     var filePath = path.join(__dirname, reqPath);
-    console.log('File path: %s', filePath);
+    //console.log('File path: %s', reqPath);
     //Does the requested file exist?
-    path.exists(filePath, function (exists) {
+    fs.exists(filePath, function (exists) {
       if (exists) {
         //Then serve the file
         console.log("Serving %s", reqPath);
         //We have the file, so go get it
-        var mimeType = mimeTypes[path.extname(filePath).split(".")[1]];
-        console.log('MIME type: %s', mimeType);
+        var mimeType = mimeTypes[path.extname(reqPath).split(".")[1]];
+        //console.log('MIME type: %s', mimeType);
         response.writeHead(200, {'Content-Type': mimeType});
         //Read the file
         var fileStream = fs.createReadStream(filePath);
         //and pipe it into the response object
         fileStream.pipe(response);
       } else {
-        console.warn("File '%s' does not exist", filePath);
+        console.warn("File '%s' does not exist", reqPath);
         response.writeHead(404, {'Content-Type': 'text/plain'});
         response.write('404: Resource not found\n');
-        response.write('Request path: ' + filePath);
+        response.write('Request path: ' + reqPath);
         response.end();
-        //todo: do I need a return here?
       }
     }); //end path.exists
   }
@@ -97,33 +113,66 @@ server.listen(8080);
 var ipAddr = os.networkInterfaces().wlan0[0].address;
 console.log('Server running at http://' + ipAddr + ':8080/');
 
+//=========================================================
+//Now start working on climate data collection and upload
+//=========================================================
 climate.on('ready', function () {
   console.log('Connected to climate module');
   // Loop forever
   setImmediate(function loop() {
     //Turn on the activity LED
     tessel.led[ACTIVITY_LED].on();
+    //Read the temperature from the climate module
     climate.readTemperature('f', function (err, temp) {
       //Store the result for access elsewhere
-      lastData.temp = temp;
-      lastData.timeStamp = Date.now();
+      lastData.temp = parseFloat(temp.toFixed(1));
+      //get the timestamp
+      var d = new Date();
+      lastData.timestamp = d.toString();
+      //Now read the humidity
       climate.readHumidity(function (err, humid) {
         //Store the result for access elsewhere
-        lastData.humidity = humid;
+        lastData.humidity = parseFloat(humid.toFixed(1));
         //Turn off the activity LED
         tessel.led[ACTIVITY_LED].off();
         //Write the results to the console
-        console.log('Degrees:', temp.toFixed(4) + 'F', 'Humidity:', humid.toFixed(4) + '%RH');
-        setTimeout(loop, 1000);
+        console.log('Degrees:', temp.toFixed(1) + ' F', 'Humidity:', humid.toFixed(1) + '% RH');
+        //should we be uploading data to Weather Underground?
+        if (Config.WU_UPLOAD) {
+          //upload the data to Weather Underground
+          //Build our data 'package'
+          var wuData = {
+            "action": "updateraw",
+            "ID": Config.WU_STATION_ID,
+            "PASSWORD": Config.WU_STATION_KEY,
+            "dateutc": "now",
+            "humidity": humid,
+            "tempf": temp
+          }
+          //Convert the object into URL format
+          var dataPath = querystring.stringify(wuData);
+          // console.log(dataPath);
+          console.log('Sending data to Weather Underground');
+          http.get(WU_URL + dataPath, function (res) {
+            // body will contain the final response
+            var body = '';
+            // Received data is a buffer, adding it to our body
+            res.on('data', function (data) {
+              body += data;
+            });
+            // After a full response, log it to the console
+            res.on('end', function () {
+              console.log('Upload completed: %s', body);
+            });
+          })
+          // If we have an error, log error to console
+            .on('error', function (e) {
+              console.error("Error: " + e.message);
+            });
+        }
+        // todo: do better loop management here
+        setTimeout(loop, 60000);
       });
     });
   });
 });
-
-function sleep(time) {
-  // Simple sleep function with promise
-  // 'borrowed' from http://stackoverflow.com/questions/951021/what-is-the-javascript-version-of-sleep
-  return new Promise((resolve) = > setTimeout(resolve, time)
-)
-  ;
-}
